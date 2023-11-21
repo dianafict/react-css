cc10f1f026df6d79d92ed4bfd346edfc1289d8f2
dianafict <105672083+dianafict@users.noreply.github.com>
2023-11-21 17:51:50 +0100

    css w komponentach porzadek foldery

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
+//import globalnych styli przed importem app
+import "./styles.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
