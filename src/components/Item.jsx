def71610e9ba7edf755aa44b9c0701d15f9b2dc3
dianafict <105672083+dianafict@users.noreply.github.com>
2023-11-21 16:50:57 +0100

    css inline2

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 export function Item({ emoji }) {
 	const [zoomed, setZoomed] = useState(false);
 
+	//class 2 words: "border-radius":"2px" or borderRadius
+
 	return (
 		<li className='item'>
 			<span className={`emoji ${zoomed ? "zoomed" : ""}`}>{emoji}</span>
 			<button
-				style={{ background: "blue", border: "5px solid yellow" }}
+				// style={{ background: "blue", border: "5px solid yellow" }}
 				className='btn'
 				onClick={() => {
 					setZoomed(wasZoomed => !wasZoomed);
